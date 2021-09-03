50b0d52e92b596008794f4d627247c2dbc21d664
Jordy Tirado Torres <jordytiradotorres20@gmail.com>
2021-09-03 16:32:37 -0500

    last details

--- js/index.js
+++ js/index.js
@@ -4,3 +4,9 @@ const nav = document.querySelector(".header__navigation");
 hamburger.addEventListener("click", (e) => {
   nav.classList.toggle("show");
 });
+
+document.body.addEventListener("click", (e) => {
+  if (e.target.className === "header__link") {
+    nav.classList.remove("show");
+  }
+});
